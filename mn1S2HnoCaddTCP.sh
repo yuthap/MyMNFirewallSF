490cb33e409eb6fdc4f142b1042c90b3e05c3c4d
yuthap <y1moretime@gmail.com>
2018-02-26 13:24:19 +0700

    Modify

--- mn1S2HnoCaddTCP.sh
+++ mn1S2HnoCaddTCP.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-s1 ovs-ofctl add-flow tcp:127.0.0.1:6654 "table=0, tcp,tcp_flags=+syn-ack,priority=9,in_port=1,actions=learn(table=0, eth_type=0x0800,ip_proto=6,in_port=2,output=NXM_OF_IN_PORT[]),output:2"
+#internal send syn only, create rule permit tcp from outside
+s1 ovs-ofctl add-flow tcp:127.0.0.1:6654 "table=0, priority=8, tcp,tcp_flags=+syn-ack,priority=9,in_port=1,actions=learn(table=0, priority-8, eth_type=0x0800,ip_proto=6,in_port=2,output=NXM_OF_IN_PORT[]),output:2"
+
+#internal can always send tcp out
+s1 ovs-ofctl add-flow tcp:127.0.0.1:6654 "table=0, tcp,priority=8,in_port=1,actions=output:2"
