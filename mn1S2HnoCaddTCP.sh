63a7abb7c7e8bbbcbe7c0209e3532b0be5a34f38
yuthap <y1moretime@gmail.com>
2018-02-26 13:36:45 +0700

    modify add TCP code

--- mn1S2HnoCaddTCP.sh
+++ mn1S2HnoCaddTCP.sh
@@ -5,3 +5,6 @@ s1 ovs-ofctl add-flow tcp:127.0.0.1:6654 "table=0, priority=8, tcp,tcp_flags=+sy
 
 #internal can always send tcp out
 s1 ovs-ofctl add-flow tcp:127.0.0.1:6654 "table=0, tcp,priority=8,in_port=1,actions=output:2"
+
+#if fin is sent from h1, delete learned rule
+s1 ovs-ofctl add-flow tcp:127.0.0.1:6654 "table=0, tcp,tcp_flags=+fin,priority=9,in_port=1,actions=learn(table=0, priority=9, delete_learned),output:2"
